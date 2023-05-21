d14ea4743074afd5b20e63b6f2741613f17dce53
micael-ricardo <micael.ricardo@outlook.com>
2023-05-20 21:35:48 -0300

    Ajuste de logica

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
 /** @var \Laravel\Lumen\Routing\Router $router */
+use App\Http\Controllers\CreditoController;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
 $router->post('/consulta-oferta-credito', 'CreditoController@consultaOfertaCredito');
-$router->post('/simulacao-oferta-credito', 'CreditoController@simulacaoOfertaCredito');
-
