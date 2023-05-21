0a30d763b36b6370b44401c7e7bee27e8fc7e520
micael-ricardo <micael.ricardo@outlook.com>
2023-05-21 10:12:20 -0300

    Correção de migrates precisa

--- database/migrations/2023_05_20_152007_create_ofertas_credito_table.php
+++ database/migrations/2023_05_20_152007_create_ofertas_credito_table.php
@@ -15,6 +15,7 @@ class CreateOfertasCreditoTable extends Migration
     {
         Schema::create('ofertas_credito', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('consulta_credito_id');
             $table->string('instituicao_financeira');
             $table->string('modalidade_credito');
             $table->decimal('valor_solicitado', 10, 2);
@@ -22,6 +23,8 @@ class CreateOfertasCreditoTable extends Migration
             $table->decimal('taxa_juros', 5, 2);
             $table->integer('quantidade_parcelas');
             $table->timestamps();
+
+            $table->foreign('consulta_credito_id')->references('id')->on('consulta_credito')->onDelete('cascade');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 /** @var \Laravel\Lumen\Routing\Router $router */
-// use App\Http\Controllers\CreditoController;
-
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
